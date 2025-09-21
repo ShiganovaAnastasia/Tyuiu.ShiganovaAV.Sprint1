d64e9462b00da25f1b178dd5d2becfae2758744c
Anastasia Shiganova <avshiganova@mail.ru>
2025-09-21 19:43:20 +0300

    .

---
+++ Tyuiu.ShiganovaAV.Sprint1.Task2.V24.Test/DataServiceTest.cs
@@ -0,0 +1,17 @@
+﻿using Tyuiu.ShiganovaAV.Sprint1.Task2.V24.Lib;
+namespace Tyuiu.ShiganovaAV.Sprint1.Task2.V24.Test
+{
+    [TestClass]
+    public sealed class DataServiceTest
+    {
+        [TestMethod]
+        public void ValidExpression()
+        {
+            DataService ds = new DataService();
+            int x = 2;
+            int y = 3;
+            var res = ds.CalculateDiffSquare(x, y);
+            Assert.AreEqual(1,res);
+        }
+    }
+}
---
+++ Tyuiu.ShiganovaAV.Sprint1.Task2.V24/Program.cs
@@ -0,0 +1,40 @@
+﻿using Tyuiu.ShiganovaAV.Sprint1.Task2.V24.Lib;
+namespace Tyuiu.ShiganovaAV.Sprint1.Task1.V13
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            DataService ds = new DataService();
+            Console.Title = "Спринт #1 |Выполнила: Шиганова А.В.| ПИНб-25-1";
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* Спринт #1                                                               *");
+            Console.WriteLine("* Тема: Базовые навыки работа в С#                                        *");
+            Console.WriteLine("* Задание #1                                                              *");
+            Console.WriteLine("* Вариант #24                                                            *");
+            Console.WriteLine("* Выполнила: Шиганова Анастасия Владимировна |ПИНб-25-1|                  *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* УСЛОВИЕ:                                                                *");
+            Console.WriteLine("* Написать программу, которая запрашивает у пользователя исходные данные, *");
+            Console.WriteLine("* вычисляет результат по формуле ( x - y ) ^ 2 и печатает его на экран.   *");
+            Console.WriteLine("*                                                                         *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* ИСХОДНЫЕ ДАННЫЕ:                                                        *");
+            Console.WriteLine("***************************************************************************");
+
+           int x;
+            Console.WriteLine("Введите значение х:");
+            x = Convert.ToInt32(Console.ReadLine());
+           int y;
+            Console.WriteLine("Введите значение y:");
+            y = Convert.ToInt32(Console.ReadLine());
+
+
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* РЕЗУЛЬТАТ:                                                              *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("Квадрат разности ( x - y ) ^ 2 = " + ds.CalculateDiffSquare(x,y));
+            Console.ReadLine();
+        }
+    }
+}

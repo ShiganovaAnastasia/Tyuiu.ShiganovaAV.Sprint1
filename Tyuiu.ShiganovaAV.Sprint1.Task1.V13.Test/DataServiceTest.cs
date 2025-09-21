5e2b746f80ffbcd293d76202d9e665c9efbc6a5a
Anastasia Shiganova <avshiganova@mail.ru>
2025-09-21 18:54:10 +0300

    .

---
+++ Tyuiu.ShiganovaAV.Sprint1.Task1.V13.Test/DataServiceTest.cs
@@ -0,0 +1,16 @@
+﻿using Tyuiu.ShiganovaAV.Sprint1.Task1.V13.Lib;
+namespace Tyuiu.ShiganovaAV.Sprint1.Task1.V13.Test
+{
+    [TestClass]
+    public sealed class DataServiceTest
+    {
+        [TestMethod]
+        public void ValidExpression()
+        {
+            DataService ds = new DataService();
+            double x = 2.0;
+            var res = ds.Calculate(x);
+            Assert.AreEqual(2,res);
+        }
+    }
+}
---
+++ Tyuiu.ShiganovaAV.Sprint1.Task1.V13/Program.cs
@@ -0,0 +1,38 @@
+﻿
+using Tyuiu.ShiganovaAV.Sprint1.Task1.V13.Lib;
+namespace Tyuiu.ShiganovaAV.Sprint1.Task1.V13
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            DataService ds = new DataService();
+            Console.Title = "Спринт #1 |Выполнила: Шиганова А.В.| ПИНб-25-1";
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* Спринт #1                                                               *");
+            Console.WriteLine("* Тема: Базовые навыки работа в С#                                        *");
+            Console.WriteLine("* Задание #1                                                              *");
+            Console.WriteLine("* Вариант #13                                                             *");
+            Console.WriteLine("* Выполнила: Шиганова Анастасия Владимировна |ПИНб-25-1|                  *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* УСЛОВИЕ:                                                                *");
+            Console.WriteLine("* Написать программу, которая запрашивает у пользователя исходные данные, *");
+            Console.WriteLine("* вычисляет результат по формуле х / (0.5 * х) и печатает его на экран.   *");
+            Console.WriteLine("*                                                                         *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* ИСХОДНЫЕ ДАННЫЕ:                                                        *");
+            Console.WriteLine("***************************************************************************");
+
+            double x;
+            Console.WriteLine("Введите значение х:");
+            x = Convert.ToDouble(Console.ReadLine());
+
+
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* РЕЗУЛЬТАТ:                                                              *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine(ds.Calculate(x));
+            Console.ReadLine();
+        }
+    }
+}

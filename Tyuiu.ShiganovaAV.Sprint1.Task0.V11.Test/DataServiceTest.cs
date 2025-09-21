da80154a3a4a568ab5b5f083ef0d5e3a9493a1c2
Anastasia Shiganova <avshiganova@mail.ru>
2025-09-21 17:59:27 +0300

    Add project files.

---
+++ Tyuiu.ShiganovaAV.Sprint1.Task0.V11.Test/DataServiceTest.cs
@@ -0,0 +1,15 @@
+﻿using Tyuiu.ShiganovaAV.Sprint1.Task0.V11.Lib;
+namespace Tyuiu.ShiganovaAV.Sprint1.Task0.V11.Test
+{
+    [TestClass]
+    public sealed class DataServiceTest
+    {
+        [TestMethod]
+        public void ValidExpression()
+        {
+            DataService ds = new DataService();
+            var res = ds.Calculate();
+            Assert.AreEqual(7, res);
+        }
+    }
+}
